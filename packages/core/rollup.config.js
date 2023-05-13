733e3baf7f0e7fb99b1d6e6d259495d8c69ff7e7
xbzhang2020 <xbzhang2020@icloud.com>
2023-05-13 17:39:25 +0800

    feat: rollup

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,9 +1,14 @@
 import typescript from '@rollup/plugin-typescript'
 
-export default {
+/**
+ * @type { import('rollup').RollupOptions }
+ */
+const config = {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
   },
   plugins: [typescript()],
 }
+
+export default config
