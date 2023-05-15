09976a93c46b2c1678c707d4c456f5c48748e5ce
xbzhang2020 <xbzhang2020@icloud.com>
2023-05-16 06:55:49 +0800

    refactor: 优化index.js 代码

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,29 +1,36 @@
-import { readFileSync } from 'node:fs'
+import { read, readFileSync } from 'node:fs'
 import { transformSync } from '@babel/core'
 import compiler from 'vue-template-compiler'
 import { transformVue } from './plugins/transform-vue.js'
 
-// 1. 读取文件内容
+const getCode = (file: string) => {
+  return readFileSync(file, 'utf-8')
+}
+
+const getScript = (code: string) => {
+  const sfc = compiler.parseComponent(code)
+  const script = sfc.script
+  return script
+}
+
+const transform = (code: string) => {
+  const res = transformSync(code, {
+    sourceType: 'module',
+    plugins: [
+      ['@babel/plugin-syntax-typescript'],
+      ['@babel/plugin-syntax-decorators', { version: '2023-01' }],
+      transformVue,
+    ],
+  })
+  return res
+}
+
 const fileName = 'node_modules/vue2-example/src/components/class-base.vue'
-const code = readFileSync(fileName, 'utf-8')
 
-// 2. 解析 script 部分
-const sfc = compiler.parseComponent(code)
-const script: any = sfc.script
-// console.log(script)
+const code = getCode(fileName)
+const _script = getScript(code)
+const res = transform(_script?.content || '')
+console.log(res?.code)
 
-// 3. 使用 babel 处理 script 部分
-const res: any = transformSync(script.content, {
-  sourceType: 'module',
-  plugins: [
-    ['@babel/plugin-syntax-typescript'],
-    ['@babel/plugin-syntax-decorators', { version: '2023-01' }],
-    transformVue,
-  ],
-})
-const newContent = res.code
-console.log(res.code)
-
-// 4. 输出结果
 // const content = `${code.slice(0, script.start)}\n${newContent}\n${code.slice(script.end)}`
 // console.log(content)
